3b3832f5f8ce5225bd451945e14c3bf1863c4112
Lamborge <maxpopelov@gmail.com>
2021-08-04 12:20:03 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace stalin_algoritm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
